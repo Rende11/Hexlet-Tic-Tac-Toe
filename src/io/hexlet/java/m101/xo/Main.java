772e3d6a5cb26b5fab932ffd495aa6c0cda9a854
Rende11 <artamonovn@gmail.com>
2015-06-15 00:53:26 +0300

    Update

---
+++ src/io/hexlet/java/m101/xo/Main.java
@@ -0,0 +1,24 @@
+package io.hexlet.java.m101.xo;
+
+import io.hexlet.java.m101.xo.controller.GameController;
+import io.hexlet.java.m101.xo.model.Board;
+import io.hexlet.java.m101.xo.model.Player;
+import io.hexlet.java.m101.xo.view.ConsoleView;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Player[]players = new Player[GameController.COUNT_OF_PLAYERS];
+        Board board = new Board();
+        GameController gameController = new GameController(GameController.GAME_NAME,players,board);
+        ConsoleView consoleView = new ConsoleView(gameController);
+
+        consoleView.start(players,board);
+
+    }
+}
+
+
+
+
